f48376522cf8965d3a74cc8bbccec0110ac0470f
oeh <oehoeh0131@gmail.com>
2020-04-17 20:30:05 +0900

    energychain

---
+++ app/src/main/java/org/techtown/energychain/LoginActivity.java
@@ -0,0 +1,26 @@
+package org.techtown.energychain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView registerButton = (TextView)findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/org/techtown/energychain/RegisterActivity.java
@@ -0,0 +1,23 @@
+package org.techtown.energychain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private ArrayAdapter adapter;
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        spinner = (Spinner)findViewById(R.id.OrgSpinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.Org, android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+}
